a0fe0f3863ca82ad9cde87fd9637e8300f37d1bd
PHANLAMANH <93565802+PHANLAMANH@users.noreply.github.com>
2023-12-27 15:57:45 +0700

    update AES

--- test.cpp
+++ test.cpp
@@ -3,46 +3,9 @@
 #include <bitset>
 #include <iomanip>
 #include <cstdint>
+#include <typeinfo>
+#include "structures.h"
 // S-box và Rcon đã được cung cấp
-const uint8_t sbox[256] = {
-    // 0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
-    0b01100011, 0b01111100, 0b01110111, 0b01111011, 0b11110010, 0b01101011, 0b01101111, 0b11000101, // 0
-    0b00110000, 0b00000001, 0b01100111, 0b00101011, 0b11111110, 0b11010111, 0b10101011, 0b01110110, // 1
-    0b11001010, 0b10000010, 0b11001001, 0b01111101, 0b11111010, 0b01011001, 0b01000111, 0b11110000, // 2
-    0b10101101, 0b11010100, 0b10100010, 0b10101111, 0b10011100, 0b10100100, 0b01110010, 0b11000000, // 3
-    0b10110111, 0b11111101, 0b10010011, 0b00100110, 0b00110110, 0b00111111, 0b11110111, 0b11001100, // 4
-    0b00110100, 0b10100101, 0b11100101, 0b11110001, 0b01110001, 0b11011000, 0b00110001, 0b00010101, // 5
-    0b00000100, 0b11000111, 0b00100011, 0b11000011, 0b00011000, 0b10010110, 0b00000101, 0b10011010, // 6
-    0b00000111, 0b00010010, 0b10000000, 0b11100010, 0b11101011, 0b00100111, 0b10110010, 0b01110101, // 7
-    0b00001001, 0b10000011, 0b00101100, 0b00011010, 0b00011011, 0b01101110, 0b01011010, 0b10100000, // 8
-    0b01010010, 0b00111011, 0b11010110, 0b10110011, 0b00101001, 0b11100011, 0b00101111, 0b10000100, // 9
-    0b01010011, 0b11010001, 0b00000000, 0b11101101, 0b00100000, 0b11111100, 0b10110001, 0b01011011, // A
-    0b01101010, 0b11001011, 0b10111110, 0b00111001, 0b01001010, 0b01001100, 0b01011000, 0b11001111, // B
-    0b11010000, 0b11101111, 0b10101010, 0b11111011, 0b01000011, 0b01001101, 0b00110011, 0b10000101, // C
-    0b01000101, 0b11111001, 0b00000010, 0b01111111, 0b01010000, 0b00111100, 0b10011111, 0b10101000, // D
-    0b01010001, 0b10100011, 0b01000000, 0b10001111, 0b10010010, 0b10011101, 0b00111000, 0b11110101, // E
-    0b10111100, 0b10110110, 0b11011010, 0b00100001, 0b00010000, 0b11111111, 0b11110011, 0b11010010  // F
-};
-
-const uint8_t rcon[256] = {
-    // 0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
-    0b10001101, 0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, // 0
-    0b10000000, 0b00011011, 0b00110110, 0b01101100, 0b11011000, 0b10101011, 0b01001101, 0b10011010, // 1
-    0b00101111, 0b01011110, 0b10111100, 0b1100011, 0b11011000, 0b10010111, 0b01110101, 0b1101010,   // 2
-    0b11010011, 0b10110011, 0b01111101, 0b11111010, 0b11101111, 0b11000101, 0b10010001, 0b00111001, // 3
-    0b01110010, 0b11100100, 0b11010011, 0b10111101, 0b01100001, 0b11000010, 0b10011111, 0b00100101, // 4
-    0b01001010, 0b10010100, 0b00110011, 0b01100110, 0b11001100, 0b10000011, 0b00011101, 0b00111010, // 5
-    0b01110100, 0b11101000, 0b11001011, 0b10001101, 0b00000001, 0b00000010, 0b00000100, 0b00001000, // 6
-    0b00010000, 0b00100000, 0b01000000, 0b10000000, 0b00011011, 0b00110110, 0b01101100, 0b11011000, // 7
-    0b10101011, 0b01001101, 0b10011010, 0b00101111, 0b01011110, 0b10111100, 0b1100011, 0b11011000,  // 8
-    0b10010111, 0b01110101, 0b1101010, 0b11010011, 0b10110011, 0b01111101, 0b11111010, 0b11101111,  // 9
-    0b11000101, 0b10010001, 0b00111001, 0b01110010, 0b11100100, 0b11010011, 0b10111101, 0b01100001, // A
-    0b11000010, 0b10011111, 0b00100101, 0b01001010, 0b10010100, 0b00110011, 0b01100110, 0b11001100, // B
-    0b10000011, 0b00011101, 0b00111010, 0b01110100, 0b11101000, 0b11001011, 0b10001101, 0b00000001, // C
-    0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000, 0b00011011, // D
-    0b00110110, 0b01101100, 0b11011000, 0b10101011, 0b01001101, 0b10011010, 0b00101111, 0b01011110, // E
-    0b10111100, 0b1100011, 0b11011000, 0b10010111, 0b01110101, 0b1101010, 0b11010011, 0b10110011    // F
-};
 
 // Hàm thực hiện hoán vị byte theo S-box
 void subBytes(std::bitset<128> &key)
@@ -56,16 +19,234 @@ void subBytes(std::bitset<128> &key)
     }
     key = new_key;
 }
+// Hàm ShiftRows tương thích với mảng mul2, mul3
+void shiftRows(std::bitset<128> &block)
+{
+    std::bitset<128> tmp_block = block;
 
-// Hàm sinh khóa k dựa trên Rcon
+    // Shift row 1 (index 0-3) by 1 byte to the left
+    for (int i = 0; i < 4; ++i)
+    {
+        std::bitset<8> byte = std::bitset<8>((block >> (i * 32) & std::bitset<128>(0xFF)).to_ulong());
+        byte = (byte << 2) | (byte >> 6); // Shift left by 1 byte
+        tmp_block |= (std::bitset<128>(byte.to_ulong()) << (i * 32));
+    }
+
+    // Shift row 2 (index 4-7) by 2 bytes to the left
+    for (int i = 4; i < 8; ++i)
+    {
+        std::bitset<8> byte = std::bitset<8>((block >> (i * 32) & std::bitset<128>(0xFF)).to_ulong());
+        byte = (byte << 4) | (byte >> 4); // Shift left by 2 bytes
+        tmp_block |= (std::bitset<128>(byte.to_ulong()) << (i * 32));
+    }
+
+    // Shift row 3 (index 8-11) by 3 bytes to the left
+    for (int i = 8; i < 12; ++i)
+    {
+        std::bitset<8> byte = std::bitset<8>((block >> (i * 32) & std::bitset<128>(0xFF)).to_ulong());
+        byte = (byte << 6) | (byte >> 2); // Shift left by 3 bytes
+        tmp_block |= (std::bitset<128>(byte.to_ulong()) << (i * 32));
+    }
+
+    // Row 4 (index 12-15) is not shifted in AES
+
+    block = tmp_block;
+}
+
+// Hàm MixColumns tương thích với mảng mul2, mul3
+// void mixColumns(std::bitset<128> &block, const std::bitset<8> mul2[], const std::bitset<8> mul3[])
+// {
+//     std::bitset<128> result;
+
+//     for (int i = 0; i < 16; i += 4)
+//     {
+//         std::bitset<8> a0 = std::bitset<8>((block >> (i * 8)).to_ulong() & 0xFF);
+//         std::bitset<8> a1 = std::bitset<8>((block >> ((i + 1) * 8)).to_ulong() & 0xFF);
+//         std::bitset<8> a2 = std::bitset<8>((block >> ((i + 2) * 8)).to_ulong() & 0xFF);
+//         std::bitset<8> a3 = std::bitset<8>((block >> ((i + 3) * 8)).to_ulong() & 0xFF);
+
+//         std::bitset<8> b0 = mul2[a0.to_ulong()] ^ mul3[a1.to_ulong()] ^ a2 ^ a3;
+//         std::bitset<8> b1 = a0 ^ mul2[a1.to_ulong()] ^ mul3[a2.to_ulong()] ^ a3;
+//         std::bitset<8> b2 = a0 ^ a1 ^ mul2[a2.to_ulong()] ^ mul3[a3.to_ulong()];
+//         std::bitset<8> b3 = mul3[a0.to_ulong()] ^ a1 ^ a2 ^ mul2[a3.to_ulong()];
+
+//         result |= (std::bitset<128>(b0.to_ulong()) << (i * 8));
+//         result |= (std::bitset<128>(b1.to_ulong()) << ((i + 1) * 8));
+//         result |= (std::bitset<128>(b2.to_ulong()) << ((i + 2) * 8));
+//         result |= (std::bitset<128>(b3.to_ulong()) << ((i + 3) * 8));
+//     }
+
+//     block = result;
+// }
+void mixColumns(std::bitset<128> &block, const std::bitset<8> mul2[], const std::bitset<8> mul3[])
+{
+    std::bitset<128> result;
+
+    for (int i = 0; i < 16; i += 4)
+    {
+        std::bitset<8> a[4];
+        for (int j = 0; j < 4; ++j)
+        {
+            a[j] = std::bitset<8>((block >> ((i + j) * 8)).to_ulong() & 0xFF);
+        }
+
+        std::bitset<8> b[4];
+        b[0] = mul2[a[0].to_ulong()] ^ mul3[a[1].to_ulong()] ^ a[2] ^ a[3];
+        b[1] = a[0] ^ mul2[a[1].to_ulong()] ^ mul3[a[2].to_ulong()] ^ a[3];
+        b[2] = a[0] ^ a[1] ^ mul2[a[2].to_ulong()] ^ mul3[a[3].to_ulong()];
+        b[3] = mul3[a[0].to_ulong()] ^ a[1] ^ a[2] ^ mul2[a[3].to_ulong()];
+
+        for (int j = 0; j < 4; ++j)
+        {
+            result |= (std::bitset<128>(b[j].to_ulong()) << ((i + j) * 8));
+        }
+    }
+
+    block = result;
+}
+
+// Hàm hoán vị byte ngược lại trong các hàng
+void invShiftRows(std::bitset<128> &block)
+{
+    std::bitset<128> tmp_block = block;
+
+    // Hàng 1 không di chuyển
+
+    // Di chuyển hàng 2 1 byte sang phải
+    for (int i = 4; i < 8; ++i)
+    {
+        int index = i * 16;
+        tmp_block[index] = block[index + 8];
+        tmp_block[index + 8] = block[index + 4];
+    }
+
+    // Di chuyển hàng 3 2 byte sang phải
+    for (int i = 8; i < 12; ++i)
+    {
+        int index = i * 16;
+        tmp_block[index] = block[index + 16];
+        tmp_block[index + 8] = block[index + 24];
+        tmp_block[index + 16] = block[index + 8];
+        tmp_block[index + 24] = block[index];
+    }
+
+    // Hàng 4 di chuyển 3 byte sang phải
+    for (int i = 12; i < 16; ++i)
+    {
+        int index = i * 16;
+        tmp_block[index] = block[index + 24];
+        tmp_block[index + 8] = block[index + 16];
+        tmp_block[index + 16] = block[index + 8];
+        tmp_block[index + 24] = block[index];
+    }
+
+    block = tmp_block;
+}
+
+// Hàm ngược lại của việc thay thế byte theo S-box
+void invSubBytes(std::bitset<128> &block)
+{
+    std::bitset<128> tmp_block = block;
+    for (int i = 0; i < 16; ++i)
+    {
+        int byte_index = i * 8;
+        std::bitset<128> byte = (block >> byte_index) & std::bitset<128>(0xFF);
+        tmp_block |= std::bitset<128>(inv_sbox[byte.to_ulong()]) << byte_index;
+    }
+    block = tmp_block;
+}
+
+// // Hàm thực hiện ngược lại việc MixColumns
+// void invMixColumns(std::bitset<128> &block)
+// {
+//     for (int i = 0; i < 16; i += 4)
+//     {
+//         std::bitset<8> col[4];
+//         for (int j = 0; j < 4; ++j)
+//         {
+//             int index = i * 8 + j * 32;
+//             col[j] = std::bitset<8>((block >> index & std::bitset<128>(0xFF)).to_ulong());
+//         }
+
+//         std::bitset<8> tmp[4];
+//         tmp[0] = mul14[col[0].to_ulong()] ^ mul11[col[1].to_ulong()] ^ mul13[col[2].to_ulong()] ^ mul9[col[3].to_ulong()];
+//         tmp[1] = mul9[col[0].to_ulong()] ^ mul14[col[1].to_ulong()] ^ mul11[col[2].to_ulong()] ^ mul13[col[3].to_ulong()];
+//         tmp[2] = mul13[col[0].to_ulong()] ^ mul9[col[1].to_ulong()] ^ mul14[col[2].to_ulong()] ^ mul11[col[3].to_ulong()];
+//         tmp[3] = mul11[col[0].to_ulong()] ^ mul13[col[1].to_ulong()] ^ mul9[col[2].to_ulong()] ^ mul14[col[3].to_ulong()];
+
+//         std::bitset<32> mixed_column;
+//         for (int j = 0; j < 4; ++j)
+//         {
+//             mixed_column |= std::bitset<32>(tmp[j].to_ulong()) << (j * 8);
+//         }
+
+//         for (int j = 0; j < 4; ++j)
+//         {
+//             int index = i * 8 + j * 32;
+//             std::bitset<32> small_bitset = mixed_column & std::bitset<32>(0xFF);
+//             std::string bitset_string = small_bitset.to_string();
+//             std::bitset<128> large_bitset(bitset_string);
+//             block ^= large_bitset << index;
+//             mixed_column >>= 8;
+//         }
+//     }
+// }
+void invMixColumns(std::bitset<128> &block)
+{
+    for (int i = 0; i < 16; i += 4)
+    {
+        std::bitset<8> col[4];
+        for (int j = 0; j < 4; ++j)
+        {
+            int index = (i + j) * 8;
+            col[j] = std::bitset<8>((block >> index).to_ulong() & 0xFF);
+        }
+
+        std::bitset<8> tmp[4];
+        tmp[0] = mul14[col[0].to_ulong()] ^ mul11[col[1].to_ulong()] ^ mul13[col[2].to_ulong()] ^ mul9[col[3].to_ulong()];
+        tmp[1] = mul9[col[0].to_ulong()] ^ mul14[col[1].to_ulong()] ^ mul11[col[2].to_ulong()] ^ mul13[col[3].to_ulong()];
+        tmp[2] = mul13[col[0].to_ulong()] ^ mul9[col[1].to_ulong()] ^ mul14[col[2].to_ulong()] ^ mul11[col[3].to_ulong()];
+        tmp[3] = mul11[col[0].to_ulong()] ^ mul13[col[1].to_ulong()] ^ mul9[col[2].to_ulong()] ^ mul14[col[3].to_ulong()];
+
+        std::bitset<32> mixed_column;
+        for (int j = 0; j < 4; ++j)
+        {
+            mixed_column |= std::bitset<32>(tmp[j].to_ulong()) << (j * 8);
+        }
+
+        for (int j = 0; j < 4; ++j)
+        {
+            int index = (i + j) * 8;
+            std::bitset<32> small_bitset = mixed_column & std::bitset<32>(0xFF);
+            block ^= std::bitset<128>(small_bitset.to_ulong()) << index;
+            mixed_column >>= 8;
+        }
+    }
+}
+
 std::bitset<128> generateKey(const std::bitset<128> &key)
 {
     std::bitset<128> new_key = key;
-    for (int i = 0; i < 11; ++i)
+
+    for (int i = 0; i < 10; ++i)
     {
-        subBytes(new_key);                    // Hoán vị byte theo S-box
-        new_key ^= std::bitset<128>(rcon[i]); // XOR với Rcon
+        // Thực hiện các bước chính của Key Expansion trong AES
+        subBytes(new_key);
+        shiftRows(new_key);
+        // mixColumns(new_key, mul2, mul3);
+
+        // XOR với Rcon
+        std::bitset<128> rcon_byte = std::bitset<128>(rcon[i]);
+
+        // XOR với phần tử trước 4 bytes (word trước) của new_key
+        std::bitset<128> tmp_key = new_key;
+        tmp_key >>= 32; // Lấy 4 byte thấp nhất
+        new_key ^= tmp_key;
     }
+
+    // Thực hiện một lần nữa SubBytes cho new_key cuối cùng trước khi trả về
+    subBytes(new_key);
+
     return new_key;
 }
 
@@ -97,11 +278,74 @@ std::vector<std::bitset<128>> divideMessageIntoBlocks(const std::string &message
 
 std::bitset<128> encryptBlock(const std::bitset<128> &generated_key, const std::bitset<128> &block)
 {
-    return generated_key ^ block; // Simple XOR encryption for demonstration purposes
+    std::bitset<128> round_key = generated_key;
+
+    // AddRoundKey: XOR khối dữ liệu với khóa ban đầu
+    std::bitset<128> encrypted_block = block ^ round_key;
+
+    // 9 vòng lặp cho AES-128 (có thể điều chỉnh cho AES khác)
+    for (int i = 0; i < 9; ++i)
+    {
+        subBytes(encrypted_block);
+        shiftRows(encrypted_block);
+        // mixColumns(encrypted_block, mul2, mul3);
+        round_key = generateKey(round_key); // Tạo khóa cho vòng lặp tiếp theo
+        encrypted_block ^= round_key;       // AddRoundKey: XOR với khóa vòng tiếp theo
+    }
+
+    // Vòng lặp cuối cùng mà không có MixColumns
+    subBytes(encrypted_block);
+    shiftRows(encrypted_block);
+    round_key = generateKey(round_key);
+    encrypted_block ^= round_key;
+
+    return encrypted_block;
 }
+
 std::bitset<128> decryptBlock(const std::bitset<128> &generated_key, const std::bitset<128> &encrypted_block)
 {
-    return generated_key ^ encrypted_block; // Simple XOR decryption for demonstration purposes
+    std::bitset<128> round_key = generated_key;
+
+    // Vòng lặp cuối cùng mà không có MixColumns (ngược lại với encryptBlock)
+    std::bitset<128> decrypted_block = encrypted_block ^ generateKey(round_key);
+
+    // 9 vòng lặp ngược lại cho AES-128 (có thể điều chỉnh cho AES khác)
+    for (int i = 8; i >= 0; --i)
+    {
+        invShiftRows(decrypted_block);
+        invSubBytes(decrypted_block);
+        round_key = generateKey(round_key); // Tạo khóa cho vòng lặp tiếp theo
+        decrypted_block ^= round_key;       // AddRoundKey: XOR với khóa vòng tiếp theo
+        invMixColumns(decrypted_block);     // Giải mã MixColumns
+    }
+
+    // AddRoundKey cuối cùng với khóa ban đầu
+    invShiftRows(decrypted_block);
+    invSubBytes(decrypted_block);
+    round_key = generateKey(round_key);
+    decrypted_block ^= round_key;
+
+    return decrypted_block;
+}
+
+std::string blocksToMessage(const std::vector<std::bitset<128>> &message_blocks, size_t original_length)
+{
+    std::string message;
+
+    for (const auto &block : message_blocks)
+    {
+        std::string block_str = block.to_string();
+        for (size_t i = 0; i < block_str.size(); i += 8)
+        {
+            std::bitset<8> byte(block_str.substr(i, 8));
+            message += static_cast<char>(byte.to_ulong());
+        }
+    }
+
+    // Chỉ lấy số ký tự ban đầu của tin nhắn
+    message = message.substr(0, original_length);
+
+    return message;
 }
 
 int main()
@@ -114,8 +358,9 @@ int main()
 
     // In ra khóa đã sinh
     std::cout << "Generated key K: " << generated_key << std::endl;
+    std::cout << typeid(generated_key).name() << std::endl;
 
-    std::string message = "this is a test"; // Tin nhắn cần chia thành các khối
+    std::string message = "this is a test, my name is Lam Anh. "; // Tin nhắn cần chia thành các khối
 
     // Chia tin nhắn thành các khối 128 bit
     std::vector<std::bitset<128>> message_blocks = divideMessageIntoBlocks(message);
@@ -129,9 +374,10 @@ int main()
     {
         std::bitset<128> encrypted_block = encryptBlock(generated_key, block);
         std::cout << "Encrypted Block: " << encrypted_block << std::endl;
-
-        // Decrypt each block using the same key
-        std::bitset<128> decrypted_block = decryptBlock(generated_key, encrypted_block);
+    }
+    for (const auto &block : message_blocks)
+    {
+        std::bitset<128> decrypted_block = decryptBlock(generated_key, block);
         std::cout << "Decrypted Block: " << decrypted_block << std::endl;
     }
     // Chuyển các khối đã giải mã thành tin nhắn ban đầu
@@ -139,4 +385,19 @@ int main()
     std::cout << "Decrypted Message: " << decrypted_message << std::endl;
 
     return 0;
+    // std::bitset<8> a(0b11011010); // Ví dụ giá trị bitset a
+    // std::bitset<8> b(0b11110000); // Ví dụ giá trị bitset b
+
+    // std::bitset<16> result = a.to_ulong() + b.to_ulong(); // Thực hiện phép nhân
+
+    // if (result.count() > 8)
+    // {
+    //     std::cout << "Tran" << std::endl;
+    // }
+    // else
+    // {
+    //     std::cout << "Khong tran." << std::endl;
+    // }
+
+    // return 0;
 }
